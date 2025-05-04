b38d8998f34a20aeeb3fd0c6bba2ddf69cce4cd8
saifFast <saifullahhakro@gmail.com>
2025-05-04 15:14:13 +0500

    added CROS support

--- CoreBackend/Program.cs
+++ CoreBackend/Program.cs
@@ -6,12 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:52302") // Angular app origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();   
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +29,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.UseCors("AllowFrontend");
 
 app.Run();
